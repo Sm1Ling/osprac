ff4d7e268c4bcde51d9c3192dc2d6b5a1d16b674
user <Azamatkhan>
2021-05-24 14:39:58 +0300

    lab13 done

---
+++ lab13.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(void)
+{
+
+char namelast[12];
+char namenew[12];
+FILE* fw;
+
+
+sprintf(namelast, "0.fff");
+fw = fopen(namelast,"w");
+fclose(fw);
+printf("Файл %s создан\n",namelast);
+
+for(int i = 1; i< 10000000; i++) {
+
+sprintf(namenew, "%d.fff", i);
+
+symlink(namelast, namenew); 
+printf("Связь с файлом %s создана\n",namenew);
+
+fw = fopen(namenew, "r");
+if(!fw){
+printf("Recursion level: %d\n",i);
+break;
+}
+fclose(fw);
+;
+strcpy(namelast, namenew);
+
+}
+
+return 0;
+}
